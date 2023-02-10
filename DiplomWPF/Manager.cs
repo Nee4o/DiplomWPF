ce893bcf48b0d7f3ab62e3a7502a79ddd73bfbdc
Максим Мартынов <martynov.max-333@yandex.ru>
2023-02-10 19:05:06 +0300

    Чуть фиксов

--- DiplomWPF/Manager.cs
+++ DiplomWPF/Manager.cs
@@ -1,13 +1,4 @@
-﻿using DiplomWPF.Models;
-using DiplomWPF.Pages;
-using DiplomWPF.Pages.AddEditPages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using Request = DiplomWPF.Pages.Request;
+﻿using System.Windows.Controls;
 
 namespace DiplomWPF
 {
